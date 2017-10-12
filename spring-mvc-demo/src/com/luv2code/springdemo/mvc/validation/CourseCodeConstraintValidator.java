9332908f68c0071c789238a76f0a7e9856b168d1
Amrut Patil <amrut.patil88@gmail.com>
2017-10-12 15:33:37 -0400

    Update CourseCodeConstraintValidator.java

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -17,7 +17,7 @@ public class CourseCodeConstraintValidator implements ConstraintValidator<Course
 	@Override
 	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {  //theCode parameters refers to the form data entered by the user on the HTML form
 		
-		boolean result;   //Check to see of the course code entered by the user starts with the prefix "LUV"
+		boolean result;   
 		
 		if(theCode != null){
 			result = theCode.startsWith(coursePrefix);
