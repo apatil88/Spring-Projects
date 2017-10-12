ec46048341d20e33fa000b08c622767a1a60e9f2
Amrut Patil <amrut.patil88@gmail.com>
2017-10-12 15:34:32 -0400

    Update CourseCodeConstraintValidator.java

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -4,18 +4,14 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
-
-	
 	private String coursePrefix;
-	
 	@Override
 	public void initialize(CourseCode theCourseCode){
-		coursePrefix = theCourseCode.value();               //Value defined in the CourseCode annotation "value" attribute
+		coursePrefix = theCourseCode.value();             
 	}
 	
-	
 	@Override
-	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {  //theCode parameters refers to the form data entered by the user on the HTML form
+	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
 		
 		boolean result;   
 		
